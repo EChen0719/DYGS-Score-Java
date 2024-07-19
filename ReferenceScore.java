4778064b940d912d645e8ac53895d87f1e65c5e9
Eric Chen <echen0719@gmail.com>
2024-07-19 13:56:43 -0400

    Create ReferenceScore.java

---
+++ ReferenceScore.java
@@ -0,0 +1,9 @@
+public class ReferenceScore {
+    public String reference_players; //Creates an array group for reference_players
+  	public double reference_scores; //Creates an array group for reference_scores
+
+public ReferenceScore(String player, double score) {
+    reference_players = player;
+    reference_scores = score;
+  }
+}
